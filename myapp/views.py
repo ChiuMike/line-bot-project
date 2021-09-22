52987d9f1d3c74f548db7079da969b1e059ccef6
Mao-Syuan <37368872+ChiuMike@users.noreply.github.com>
2021-09-22 16:32:03 +0800

    test

---
+++ myapp/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from django.conf import settings
+from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
+from django.views.decorators.csrf import csrf_exempt
+import os
+from linebot import LineBotApi, WebhookParser
+from linebot.exceptions import InvalidSignatureError, LineBotApiError
+from linebot.models import MessageEvent, TextSendMessage
+
+# from invoiceapi.models import users
+# from module import func
+
+# Create your views here.
+line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
+parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
+@csrf_exempt
+def callback(request):
+    if request.method == 'POST':
+        signature = request.META['HTTP_X_LINE_SIGNATURE']
+        body = request.body.decode('utf-8')
+        try:
+            events = parser.parse(body, signature)
+        except InvalidSignatureError:
+            return HttpResponseForbidden()
+        except LineBotApiError:
+            return HttpResponseBadRequest()
+
+        for event in events:
+            if isinstance(event, MessageEvent):
+                mtext = event.message.text
+                if mtext == '幹':
+                    texe="你好"
+                    line_bot_api.reply_message(event.reply_token,texe);
+        return HttpResponse()
+
+    else:
+        return HttpResponseBadRequest()
